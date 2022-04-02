550155e59ef0f303c7dca9d64c71a6c3e1e0304e
dhh1128 <daniel.hardman@gmail.com>
2022-04-02 17:38:36 +0200

    add ability to fuzzy match book names
    Signed-off-by: dhh1128 <daniel.hardman@gmail.com>

--- cite2link/cite.py
+++ cite2link/cite.py
@@ -1,34 +1,26 @@
 import re
 
+
 def transform(txt):
     return txt
 
 
-scripture_ref_pat = re.compile(r"""
-    (first|1(?:st)?|2(?:nd)?|third|3(?:rd)?|fourth|4(?:th)?)\s* # leading volume, cap group 1
-    ([a-z ]+) # book/author, cap group 2
-    \W*
-    (\d+) # chapter (or item, for some sources), cap group 3
-    ( # everything after chapter/item is optional
-    \s*:\s*
-    ([-0-9, ]+) # verses
-    )? # end of optional part
-    """, re.I | re.VERBOSE
-)
-
-
 word_splitter = re.compile(r'(\w)(\w*)(\W|$)')
+
+
 def title_case(txt):
     capitalized = ''
     for m in word_splitter.finditer(txt):
         capitalized += m.group(1).upper() + m.group(2) + m.group(3)
-    return capitalized.replace(' Of ', ' of ')
+    return capitalized.replace(' Of ', ' of ').replace(' And ', ' and ')
+
 
 class Book:
     def __init__(self, txt):
+        txt = txt.lower().strip()
         if txt[0].isdigit():
             self.ordinal = txt[0]
-            txt = txt[2:]
+            txt = txt[1:].lstrip()
         else:
             self.ordinal = None
         self.unique = None
@@ -43,7 +35,7 @@ class Book:
             txt = txt[:i]
             if not self.unique:
                 self.unique = self.abbrev
-        names = txt.split('|')
+        names = txt.replace('.', '').split('|')
         self.names = names[:]
         if not self.unique:
             self.unique = names[0]
@@ -54,16 +46,19 @@ class Book:
                 acronym = ''.join([w[0] for w in words])
                 if acronym not in self.names:
                     self.names.append(acronym)
+        self.first_chars = set([x[0] for x in self.names])
 
     @property
     def title(self):
         t = title_case(self.names[0])
         return self.ordinal + ' ' + t if self.ordinal else t
 
-
     @property
     def unique_basis(self):
         return self.ordinal + self.names[0] if self.ordinal else self.names[0]
+
+    def __str(self):
+        return self.title
 
 
 def load(x):
@@ -83,17 +78,95 @@ nt_books = load('matthew:matt/mat,mark/mar,luke/lu,john/joh,acts/ac,romans:rom/r
     'titus/ti,philemon:philem/phile,hebrews:heb,james/jam,1 peter:pet/1pe,2 peter:pet/2pe,1 john:jn/1jo,' +
     '2 john:jn/2jo,3 john:jn/3jo,jude,revelation|apocalypse:rev/re')
 
+bible = [ot_books, nt_books]
+
 bom_books = load('1 nephi:ne/1ne,2 nephi:ne/2ne,jacob/jac,enos/en,jarom/jar,omni/om,words of mormon/wo,' +
     'mosiah/mosi,alma/al,helaman:hel,3 nephi:ne/3ne,4 nephi:ne/4ne,mormon:morm,ether/et,moroni:moro')
 
 pgp_books = load('moses:mos/mose,abraham:abr/ab,joseph smith-matthew:js-m/joseph smith-m,' +
     'joseph smith-history:js-h/joseph smith-h,articles of faith:a of f/ar')
 
-all_books = [ot_books, nt_books, bom_books, pgp_books]
+doctrine_and_covenants = load('doctrine and covenants|doctrine & covenants|dc:d&c/do,official declaration/of')
+
+quad_books = [bible, bom_books, doctrine_and_covenants, pgp_books]
+
+all_books = quad_books
 
 del load
 
-def split_scripture_ref(ref):
-    m = scripture_ref_pat.match(ref)
+lead_ordinal_pat = re.compile(r'(first|1(?:st)?|sec(?:ond)?|2(?:nd)?|third|3(?:rd)?|fourth|4(?:th)?)\s*(.*)', re.I)
+
+scripture_cite_pat = re.compile(r"""
+    (first|1(?:st)?|2(?:nd)?|third|3(?:rd)?|fourth|4(?:th)?)\s* # leading volume, cap group 1
+    ([a-z ]+) # book/author, cap group 2
+    \W*
+    (\d+) # chapter (or item, for some sources), cap group 3
+    ( # everything after chapter/item is optional
+    \s*:\s*
+    ([-0-9, ]+) # verses
+    )? # end of optional part
+    """, re.I | re.VERBOSE
+)
+
+def normalize_ordinal(ordinal):
+    return ordinal[0] if ordinal[0].isdigit() else str('ieho'.index(ordinal[1].lower()) + 1)
+
+def find_book(book_name_in_ref):
+    # Normalize punctuation in what we were given.
+    if '&' in book_name_in_ref:
+        book_name_in_ref = book_name_in_ref.replace(
+            '&amp;', '&').replace(
+            '&mdash;','-').replace(
+            '&#151;', '-').replace(
+            '&ndash;', '-').replace(
+            '&#150;', '-')
+    # Normalize stuff like "First Chron" and "3rd Esdras"
+    m = lead_ordinal_pat.match(book_name_in_ref)
+    if m: # Transform into "1 Chron" or "3 Esdras" for lookup
+        book_name_in_ref = normalize_ordinal(m.group(1)) + m.group(2)
+    named = Book(book_name_in_ref)           # imagine book_name_in_ref = "1 Chron."
+    name = named.names[0]                    # "chron"
+    first_char = name[0]
+    for book in next_book(all_books):
+        # First test: make sure both books lack an ordinal -- or that both
+        # books have one, and the ordinals match.
+        if named.ordinal == book.ordinal:
+            # Second test: does name match the first letter of any variant of
+            # this book's name? If no, there's no point in doing fancier
+            # comparisons.
+            if first_char in book.first_chars:
+                # If we got a canonical abbreviation, it's a match.
+                if name == book.abbrev:
+                    return book
+                # If we got an exact book name, it's a match, too.
+                for n in book.names:
+                    if name == n:
+                        return book
+                # Do we have an unfamiliar short form (e.g., "Genes" for "Genesis")?
+                x = len(named.unique_basis)
+                if x >= len(book.unique) and named.unique_basis.startswith(book.unique):
+                    # We now know that the referenced book has a name that starts with the string that
+                    # uniquely identifies a particular book in our library. However, what if our reference
+                    # differs from this unique value later on? For example, "ge" uniquely identifies
+                    # genesis. If we get a reference to a book named "gesture", we don't want to match.
+                    if named.unique_basis == book.unique_basis[:x]:
+                        return book
+                    # If we get here, we definitely won't find another match, because we've found that
+                    # our string matched something unique to one of the books in our library -- yet didn't
+                    # match it all the way. We can abort our loop and just return None.
+                    return
+
+def next_book(container):
+    for item in container:
+        if isinstance(item, Book):
+            yield item
+        else:
+            # Recurse
+            for book in next_book(item):
+                yield book
+
+
+def parse(ref):
+    m = scripture_cite_pat.match(ref)
     if m:
         return m.group(1), m.group(2)
--- cite2link/tests/cite_test.py
+++ cite2link/tests/cite_test.py
@@ -10,20 +10,25 @@ def assert_book(book, name, ordinal, abbrev, *names):
         for n in names:
             assert n in other_names
 
+
 def test_book_counts():
     assert len(ot_books) == 39
     assert len(nt_books) == 27
     assert len(bom_books) == 15
     assert len(pgp_books) == 5
 
+
 def test_easy_book():
     assert_book(nt_books[0], 'matthew', None, 'matt')
+
 
 def test_ordinal_book():
     assert_book(bom_books[10], 'nephi', '3', 'ne')
 
+
 def test_hard_book():
     assert_book(ot_books[21], 'song of solomon', None, 'song', 'sos', 'song of songs', 'canticles')
+
 
 def test_uniques():
 
@@ -41,25 +46,62 @@ def test_uniques():
 
     def uniques_are_perfect():
         perfect = True
-        for collection in all_books:
-            for book in collection:
-                name = book.unique_basis
-                common_len = 2 if book.ordinal else 1
-                # Look at all other books; see which has the most letters in common
-                # with the name of this one.
-                for collection2 in all_books:
-                    for book2 in collection2:
-                        if book2 != book:
-                            name2 = book2.unique_basis
-                            common_len = max(common_len, count_common(name, name2))
-                expected_unique = name[:common_len + 1]
-                if book.unique != expected_unique:
-                    print('Expected unique for %s to be "%s"' % (book.title, expected_unique))
-                    perfect = False
+        for book in next_book(all_books):
+            name = book.unique_basis
+            common_len = 2 if book.ordinal else 1
+            # Look at all other books; see which has the most letters in common
+            # with the name of this one.
+            for book2 in next_book(all_books):
+                if book2 != book:
+                    name2 = book2.unique_basis
+                    common_len = max(common_len, count_common(name, name2))
+            expected_unique = name[:common_len + 1]
+            if book.unique != expected_unique:
+                print('Expected unique for %s to be "%s"' % (book.title, expected_unique))
+                perfect = False
         return perfect
 
     assert uniques_are_perfect()
 
 
+def assert_found(lookup, title):
+    x = find_book(lookup)
+    assert x
+    assert x.title == title
 
+
+def test_books_found():
+    assert_found('eze', 'Ezekiel')
+    assert_found('do', 'Doctrine and Covenants')
+    assert_found('d&amp;c', 'Doctrine and Covenants')
+    assert_found('d&c', 'Doctrine and Covenants')
+    assert_found('dc', 'Doctrine and Covenants')
+    assert_found('lu', 'Luke')
+    assert_found('Matthe', 'Matthew')
+    assert_found('sos', 'Song of Solomon')
+    assert_found('Canticles', 'Song of Solomon')
+    assert_found('1jn', '1 John')
+    assert_found('ThirdJohn.', '3 John')
+    assert_found('2nd Pet.', '2 Peter')
+    assert_found('4th nephi', '4 Nephi')
+    assert_found('3 ne.', '3 Nephi')
+    assert_found('1chr', '1 Chronicles')
+    assert_found('1 chron', '1 Chronicles')
+    assert_found('genesis', 'Genesis')
+
+
+def test_books_not_found():
+    # Not enough characters for reliable match
+    assert find_book('m') is None
+    # Too many characters
+    assert find_book('matthewx') is None
+    # Partial name that isn't canonical and therefore not truncatable
+    assert find_book('Cantic') is None
+
+
+def xtest_parse():
+    x = parse("1 Ne 3:7")
+    assert x.book.title == "1 Ne"
+    assert x.chapter == '3'
+    assert x.verses == ['7']
 
