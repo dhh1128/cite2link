1792674085c5d993b30bbc0570e1de30f4ccd614
dhh1128 <daniel.hardman@gmail.com>
2022-04-02 21:47:41 +0200

    improve parsing; normalize verses
    Signed-off-by: dhh1128 <daniel.hardman@gmail.com>

--- cite2link/cite.py
+++ cite2link/cite.py
@@ -17,6 +17,7 @@ def title_case(txt):
 
 class Book:
     def __init__(self, txt):
+        self.chapter_and_verse = True
         txt = txt.lower().strip()
         if txt[0].isdigit():
             self.ordinal = txt[0]
@@ -36,6 +37,9 @@ class Book:
             if not self.unique:
                 self.unique = self.abbrev
         names = txt.replace('.', '').split('|')
+        if names[0].endswith('!'):
+            self.chapter_and_verse = False
+            names[0] = names[0][:-1]
         self.names = names[:]
         if not self.unique:
             self.unique = names[0]
@@ -65,6 +69,14 @@ def load(x):
     return [Book(item) for item in x.split(',')]
 
 
+# Define collections of books. Each item is in the format:
+#       long name|alternate name|another alt:can/unique
+# ...where "can" means a canonical abbreviation, and "unique"
+# means the shortest string that uniquely identifies the book
+# in the corpus of all book names. If the first long name for
+# a book ends with !, this means the book doesn't have
+# both chapter and verse (it's cited with a single number).
+
 ot_books = load('genesis:gen/ge,exodus:ex,leviticus:lev/le,numbers:num/nu,deuteronomy:deut/de,joshua:josh,' +
     'judges:judg,ruth/ru,1 samuel:sam/1sa,2 samuel:sam/2sa,1 kings:kgs/1ki,2 kings:kgs/2ki,1 chronicles:chr/1ch,' +
     '2 chronicles:chr/2ch,ezra/ezr,nehemiah:neh/ne,esther:esth/es,job,psalms:ps,proverbs:prov/pr,' +
@@ -84,9 +96,9 @@ bom_books = load('1 nephi:ne/1ne,2 nephi:ne/2ne,jacob/jac,enos/en,jarom/jar,omni
     'mosiah/mosi,alma/al,helaman:hel,3 nephi:ne/3ne,4 nephi:ne/4ne,mormon:morm,ether/et,moroni:moro')
 
 pgp_books = load('moses:mos/mose,abraham:abr/ab,joseph smith-matthew|jsm|jsmatthew|jsmatt|js-matthew:js-m/joseph smith-m,' +
-    'joseph smith-history|jsh|jshistory|jshist|js-history:js-h/joseph smith-h,articles of faith|art of faith|art faith|af:a of f/ar')
+    'joseph smith-history|jsh|jshistory|jshist|js-history:js-h/joseph smith-h,articles of faith!|art of faith|art faith|af:a of f/ar')
 
-doctrine_and_covenants = load('doctrine and covenants|doctrine & covenants|dc:d&c/do,official declaration/of')
+doctrine_and_covenants = load('doctrine and covenants|doctrine & covenants|dc:d&c/do,official declaration!/of')
 
 quad_books = [bible, bom_books, doctrine_and_covenants, pgp_books]
 
@@ -94,12 +106,22 @@ all_books = quad_books
 
 del load
 
+
 lead_ordinal_pat = re.compile(r'(first|1(?:st)?|sec(?:ond)?|2(?:nd)?|third|3(?:rd)?|fourth|4(?:th)?)\s*(.*)', re.I)
 
+
 def normalize_ordinal(ordinal):
+    """
+    Given a string like "first" or "1st" or "1", return the canonical version ('1').
+    """
     return ordinal[0] if ordinal[0].isdigit() else str('ieho'.index(ordinal[1].lower()) + 1)
 
+
 def find_book(book_name_in_ref):
+    """
+    Look through all known books and find one that matches the given book name.
+    Do fuzzy matching. Return the matching book if found, else None.
+    """
     # Normalize punctuation in what we were given.
     if '&' in book_name_in_ref:
         book_name_in_ref = book_name_in_ref.replace(
@@ -146,6 +168,9 @@ def find_book(book_name_in_ref):
 
 
 def next_book(container):
+    """
+    Yield all individual books in a list or list of lists or similar.
+    """
     for item in container:
         if isinstance(item, Book):
             yield item
@@ -156,8 +181,8 @@ def next_book(container):
 
 
 scripture_cite_pat = re.compile(r"""
-    ((?:first|1(?:st)?|sec(?:ond)?|2(?:nd)?|third|3(?:rd)?|fourth|4(?:th)?)\s* # leading volume
-    (?:[a-z]+(?:\ +[a-z])*)) # book/author, cap group 1
+    ((?:first|1(?:st)?|sec(?:ond)?|2(?:nd)?|third|3(?:rd)?|fourth|4(?:th)?)?\s* # leading volume
+    (?:[a-z.]+(?:\ +[a-z.])*)) # book/author, cap group 1
     \W*
     (\d+) # chapter (or item, for some sources), cap group 2
     (?: # everything after chapter/item is optional
@@ -169,6 +194,102 @@ scripture_cite_pat = re.compile(r"""
 
 
 def parse(ref):
+    """
+    See if a scriptural reference can be recognized as matching a standard format.
+    If yes, return a tuple of (book, chapter, verse). If no, return None.
+    This does syntactic analysis only; it makes no attempt to see if the book name
+    is valid or the chapter and verse portion make sense.
+    """
     m = scripture_cite_pat.match(ref)
     if m:
         return m.group(1), m.group(2), m.group(3)
+
+
+verse_range_pat = re.compile(r'(\d+)-(\d+)')
+verse_splitter_pat = re.compile(r'[ ,]+')
+space_range_pat = re.compile(r' +-')
+range_space_pat = re.compile(r'- +')
+
+
+def split_verses(verses):
+    """
+    Split on any runs of commas and spaces. Remove spaces.
+    """
+    verses = verse_splitter_pat.split(range_space_pat.sub('-', space_range_pat.sub('-', verses)))
+    return [v for v in verses if v]
+
+
+def get_nums_and_pairs_from_verses_text(verses):
+    """
+    Given an array of strings that describe either individual verses
+    or ranges of verses, return an array of corresponding ints and
+    int pairs: '3','5', '7-10' --> [3,5,(7,10)]
+    """
+    items = []
+    # Get components -- either individual verses, or verse ranges.
+    # Convert them to integers or tuples that hold integer pairs.
+    for item in verses:
+        m = verse_range_pat.match(item)
+        if m:
+            pair = (int(m.group(1)), int(m.group(2)))
+            if pair[0] > pair[1]:
+                raise Exception('Bad range "%s"; %s > %s.' % (item, m.group(1), m.group(2)))
+            items.append(pair)
+        else:
+            items.append(int(item))
+    return items
+
+
+def join_nums_and_pairs(verses):
+    """
+    Given an array of ints and int pairs, return a single string
+    of individual verse numbers and verse ranges: [3,5,(7,10)] --> "3, 5, 7-10".
+    """
+    return ', '.join([str(x) if isinstance(x, int) else '%d-%d' % x for x in verses])
+
+
+def normalize_verses(verses):
+    """
+    Put verses in a canonical format -- ordered, with no redundancies or overlaps,
+    and with maximum use of ranges for terseness. Return an array of ints and
+    int pairs. To convert to text, call join_nums_and_pairs().
+    """
+    verses = split_verses(verses)
+    nums_and_pairs = get_nums_and_pairs_from_verses_text(verses)
+    # Sort them.
+    verses = sorted(nums_and_pairs, key=lambda x: x if isinstance(x, int) else x[0])
+    # Now go through the verses and look for redundancies, overlaps, or
+    # consecutive verses that should be ranges. Rewrite as needed.
+    nums_and_pairs = []
+    start = end = -1
+    for item in verses:
+        is_num = isinstance(item, int)
+        new = item if is_num else item[0]
+        # Do we have a gap in verses that justifies adding a new item to the normalized list?
+        if (new > end + 1) or not nums_and_pairs:
+            start = new
+            if is_num:
+                end = start
+                nums_and_pairs.append(start)
+            else:
+                end = item[1]
+                nums_and_pairs.append((start, end))
+        else:
+            extend = False
+            prev = nums_and_pairs[-1]
+            prev_is_num = isinstance(prev, int)
+            # Do we have a value that's one bigger than the last verse we saw? This might be
+            # relatively common; a citation might list verses like "1, 2-3" when it should give
+            # a range "1-3".
+            if new == end + 1:
+                extend = True
+            # What about an overlapping range?
+            elif not is_num:
+                if item[1] > end:
+                    extend = True
+            if extend:
+                old_start = prev if prev_is_num else prev[0]
+                nums_and_pairs[-1] = (old_start, new) if is_num else (old_start, item[1])
+                end = nums_and_pairs[-1][1]
+            # else this item is totally redundant
+    return nums_and_pairs
--- cite2link/tests/cite_test.py
+++ cite2link/tests/cite_test.py
@@ -1,3 +1,5 @@
+from pytest import raises
+
 from ..cite import *
 
 
@@ -78,6 +80,7 @@ def test_books_found():
     assert_found('jshistory', 'Joseph Smith-History')
     assert_found('joseph smith-m', 'Joseph Smith-Matthew')
     assert_found('js-m', 'Joseph Smith-Matthew')
+    assert_found('js&mdash;m', 'Joseph Smith-Matthew')
     assert_found('jsm', 'Joseph Smith-Matthew')
     assert_found('eze', 'Ezekiel')
     assert_found('do', 'Doctrine and Covenants')
@@ -115,6 +118,34 @@ def assert_parse(ref, book, chapter, verse):
     assert ch == chapter
     assert v == verse
 
+
 def test_parse():
+    assert_parse("eph  4", 'eph', '4', None)
+    assert_parse("1jn.01:06-10", '1jn.', '01', '06-10')
+    assert_parse("Gen 33:1, 3-4", 'Gen', '33', '1, 3-4')
     assert_parse("1 Ne 3:7", '1 Ne', '3', '7')
 
+
+def assert_norm(input, output):
+    assert join_nums_and_pairs(normalize_verses(input)) == output
+
+
+def test_normalize_verses():
+    assert_norm('4 - 12 7', '4-12')
+    assert_norm('1-3 2, 3, 1, 1', '1-3')
+    assert_norm('1-3 2-4', '1-4')
+    assert_norm('01', '1')
+    assert_norm('1,', '1')
+    assert_norm(', 1 3,,, 2', '1-3')
+    assert_norm('1,2 3', '1-3')
+    assert_norm('1', '1')
+
+
+def assert_bad(input):
+    with raises(Exception):
+        normalize_verses(input)
+
+
+def test_unnormalizable_verses():
+    assert_bad('3-')
+    assert_bad('3-1 2')
