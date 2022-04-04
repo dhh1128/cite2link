5bb22a627d938ede04aa5e0425a203ab6d9bc1f4
dhh1128 <daniel.hardman@gmail.com>
2022-04-04 18:58:17 +0200

    make app run even if symbolically linked
    Signed-off-by: dhh1128 <daniel.hardman@gmail.com>

--- cite2link/app.py
+++ cite2link/app.py
@@ -10,16 +10,16 @@ help_pat = re.compile(r'[?]|--?h(elp)?', re.I)
 def main(argv):
     if len(argv) < 2 or len(argv) == 2 and help_pat.match(argv[1]):
         print('cite2link Genesis 1:1 (or any other scripture reference)\n')
-        sys.exit(0)
     else:
         ref = ' '.join(argv[1:])
-    try:
-        book, chapter, verses = resolve(ref)
-        print_all(book, chapter, verses)
-        sys.exit(0)
-    except:
-        sys.stderr.write("Can't resolve reference; check syntax or name of book.")
-        sys.exit(1)
+        try:
+            book, chapter, verses = resolve(ref)
+            print_all(book, chapter, verses)
+        except:
+            import traceback
+            traceback.print_exc()
+            sys.stderr.write("Can't resolve reference; check syntax or name of book.")
+            sys.exit(1)
 
 
 if __name__ == '__main__':
